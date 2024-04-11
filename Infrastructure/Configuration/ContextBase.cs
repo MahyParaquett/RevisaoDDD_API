30f505941e3f31e7ace3099b94e2c8a9b2b02a2b
Mahy Paraquett <mahyaraparaquett@gmail.com>
2024-04-11 15:59:59 -0300

    Parte 3: configuração para acessar o banco

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        //Configuração de conexão com o banco
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        //Configuração para mapear a chave primaria do Identity
+        //serve para que o Identity consiga ser visto já que modificamos ele no ApplicatioUser
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "server = T2M0348; database = RevisaoDDD; trusted_Connection = true; trustservercertificate = true";
+        }
+
+    }
+}
